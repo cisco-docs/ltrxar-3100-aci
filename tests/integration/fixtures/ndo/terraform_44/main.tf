eaac75965b0d57e9644668dac63111d8b86e3b1f
danischm <danischm@cisco.com>
2025-07-23 19:06:35 +0200

    Temporarily use mso provider version 1.4.0 for testing

--- tests/integration/fixtures/ndo/terraform_44/main.tf
+++ tests/integration/fixtures/ndo/terraform_44/main.tf
@@ -11,7 +11,7 @@ provider "mso" {
 }
 
 module "ndo" {
-  source = "github.com/netascode/terraform-mso-nac-ndo.git?ref=main"
+  source = "github.com/netascode/terraform-mso-nac-ndo.git?ref=ds-test-provider-1-4"
 
   yaml_directories = ["../standard", "../standard_44"]
 
